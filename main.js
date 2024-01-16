fd72842be3665686dc8ac5e2357f0052346016fc
Peter00010 <mwongela.oboe@gmail.com>
2024-01-16 19:31:14 +0300

    Few JS updates

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    const search = document.getElementById("search-music")
-    const songName = document.getElementById("songname")
-    const artist = document.getElementById("artist")
-    const mute = document.getElementById("mute")
-    const unmute = document.getElementById("unmute")
-    const volumeDecrease = document.getElementById("volume-decrease")
-    const volumeIncrease = document.getElementById("volume-increase")
-    const repeat = document.getElementById("repeat")
-    const play = document.getElementById("play")
-    const like = document.getElementById("like")
-    const newsletterInput = document.getElementById("newsletter-input")
-    const submitNewsletter = document.getElementById("newsletter-btn")
-    const a = document.getElementById("")
-    const a = document.getElementById("")
-
-} )
+document.addEventListener("DOMContentLoaded", () => {
+  const search = document.getElementById("search-music");
+  const songName = document.getElementById("songname");
+  const artist = document.getElementById("artist");
+  const mute = document.getElementById("mute");
+  const unmute = document.getElementById("unmute");
+  const volumeDecrease = document.getElementById("volume-decrease");
+  const volumeIncrease = document.getElementById("volume-increase");
+  const repeat = document.getElementById("repeat");
+  const play = document.getElementById("play");
+  const like = document.getElementById("like");
+  const newsletterInput = document.getElementById("newsletter-input");
+  const submitNewsletter = document.getElementById("newsletter-btn");
+});
